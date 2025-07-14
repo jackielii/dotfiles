8db41ecd1df893244a453af005d72b935971d129
Jackie Li <jackie.li@target-energysolutions.com>
2025-07-14 22:30:45 +0100

    .

--- .config/kitty/inactive_border.py
+++ .config/kitty/inactive_border.py
@@ -1,5 +1,3 @@
-from typing import Any, Dict
-
 from kitty.borders import Border, BorderColor, draw_edges
 from kitty.boss import Boss
 from kitty.fast_data_types import current_focused_os_window_id, mark_os_window_dirty
@@ -8,30 +6,35 @@ from kitty.window import DynamicColor, Window
 active_border = BorderColor.active
 inactive_border = BorderColor.inactive
 
-def on_focus_change(boss: Boss, window: Window, data: Dict[str, Any])-> None:
-    print(data)
-    tab = window.tabref()
-    all_windows = tab.windows
-    active_group = all_windows.active_group
-    wg = active_group
-    # wg = tab.windows
-    # print("currennt focused os window id:", current_focused_os_window_id())
-    # print("current os window id:", tab.os_window_id)
+current_os_window_id = None
 
-    os_window_id = tab.os_window_id
-    tab_id = tab.id
-    color = inactive_border
-    # draw_edges(os_window_id, tab_id, (color, color, color, color), wg, borders=True)
 
-    if tab.os_window_id != current_focused_os_window_id():
-        color = BorderColor.inactive
-        tab.relayout_borders()
-        draw_edges(os_window_id, tab_id, (color, color, color, color), wg, borders=True)
-        mark_os_window_dirty(os_window_id)
-    elif data["focused"]:
-        color = BorderColor.active
-        draw_edges(os_window_id, tab_id, (color, color, color, color), wg, borders=True)
-        # tab.relayout_borders()
+def on_focus_change(boss: Boss, window: Window, data: dict[str, any]) -> None:
+    global current_os_window_id
+    if current_os_window_id != current_focused_os_window_id():
+        current_os_window_id = current_focused_os_window_id()
+
+        tab = window.tabref()
+        os_window_id = tab.os_window_id
+        tab_id = tab.id
+        all_windows = tab.windows
+        active_group = all_windows.active_group
+        wg = active_group
+        # wg = tab.windows
+        # print("currennt focused os window id:", current_focused_os_window_id())
+        # print("current os window id:", tab.os_window_id)
+
+        if data["focused"]:
+            color = BorderColor.active
+            draw_edges(
+                os_window_id, tab_id, (color, color, color, color), wg, borders=True
+            )
+        else:
+            color = BorderColor.inactive
+            draw_edges(
+                os_window_id, tab_id, (color, color, color, color), wg, borders=True
+            )
+
         mark_os_window_dirty(os_window_id)
 
     # print("active border:", BorderColor.active)
@@ -47,4 +50,3 @@ def on_focus_change(boss: Boss, window: Window, data: Dict[str, Any])-> None:
     #     window.
     # else:
     #     window.change_colors({DynamicColor.default_bg: "#333"})
-
